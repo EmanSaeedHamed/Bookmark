32ef0d72cc39f5ec6c8082a3ef160c174c1e6e0c
Eman Saeed <emansaeedhamed22220@gmail.com>
2025-10-19 14:14:58 +0300

    third

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ var regex = {
   },
   siteLink: {
     value:
-      /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      /^(https:\/\/|http:\/\/)[\S]+(\.com)$/,
     isValid: false,
   },
 };
